acfd931540820b321180297a6b93249a1670d6bb
Pablo Alan <pabloxt14@gmail.com>
2025-02-17 20:48:52 -0300

    chore: initial setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
 export default function Home() {
   return (
-    <main>
-      <div>Hello world!</div>
+    <main className="p-4">
+      <button
+        type="button"
+        className="bg-violet-500 px-4 py-2 text-white rounded-md"
+      >
+        Click me
+      </button>
     </main>
-  );
+  )
 }
