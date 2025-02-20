86da9c9ae4c88da2dd7974295cff63f5dfa59bcd
Pablo Alan <pabloxt14@gmail.com>
2025-02-19 21:25:39 -0300

    feat: create input component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
-import { ArrowRight, Copy } from 'lucide-react'
+import { ArrowRight, Copy, User } from 'lucide-react'
 
 import { Button } from '@/components/button'
 import { IconButton } from '@/components/icon-button'
+import { Input, InputField, InputIcon } from '@/components/input'
 
 export default function Home() {
   return (
@@ -13,6 +14,18 @@ export default function Home() {
       <IconButton>
         <Copy size={20} />
       </IconButton>
+
+      <Input>
+        <User size={20} />
+        <InputField type="text" placeholder="Nome" />
+      </Input>
+
+      <Input error={true}>
+        <InputIcon>
+          <User size={20} />
+        </InputIcon>
+        <InputField type="email" placeholder="Email" />
+      </Input>
     </main>
   )
 }
---
+++ src/components/input.tsx
@@ -0,0 +1,52 @@
+import type { ComponentProps } from 'react'
+import { twMerge } from 'tailwind-merge'
+
+type InputProps = ComponentProps<'div'> & {
+  error?: boolean
+}
+
+function Input({ className, error, ...props }: InputProps) {
+  return (
+    <div
+      data-error={!!error}
+      className={twMerge(
+        'group w-full flex items-center gap-2 bg-gray-800 px-4 h-12 rounded-xl border border-gray-600  focus-within:border-gray-100',
+        '[&:not(:has(input:placeholder-shown))]:text-gray-100',
+        'data-[error=true]:border-danger',
+        className
+      )}
+      {...props}
+    />
+  )
+}
+
+type InputIconProps = ComponentProps<'span'>
+
+function InputIcon({ className, ...props }: InputIconProps) {
+  return (
+    <span
+      className={twMerge(
+        'text-gray-400',
+        'group-focus-within:text-gray-100 group-[&:not(:has(input:placeholder-shown))]:text-gray-100 group-data-[error=true]:text-danger!',
+        className
+      )}
+      {...props}
+    />
+  )
+}
+
+type InputFieldProps = ComponentProps<'input'>
+
+function InputField({ className, ...props }: InputFieldProps) {
+  return (
+    <input
+      className={twMerge(
+        'flex-1 bg-transparent outline-none text-gray-100 placeholder:text-gray-400',
+        className
+      )}
+      {...props}
+    />
+  )
+}
+
+export { Input, InputField, InputIcon }
